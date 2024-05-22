601eac1eb848158a6dc2e90900c0d9d576b14a4b
Raihan Jauhari <raihanfg565@gmail.com>
2024-05-22 12:43:13 +0700

    merevisi commit perkalian

--- fungsi.js
+++ fungsi.js
@@ -1,11 +1,13 @@
+console.log("Fungsi Pertama")
+
 const add = (a, b) => a + b
 console.log(add(1, 2))
 
 const subtract = (a, b) => a - b
 console.log(subtract(5, 2))
 
-const multiply = (a, b) => a * b
-console.log(multiply(2, 2))
+const muliplyNumber = (a, b) => a * b
+console.log(muliplyNumber(2, 2))
 
 const divide = (a, b) => a / b
 console.log(divide(4, 2))
